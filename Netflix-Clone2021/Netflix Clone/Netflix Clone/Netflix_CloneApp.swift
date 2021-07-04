f940dda6801f82afdffb3e90260f7eeb7f46bfab
RayQuan Kennedy <50650716+RaymanTheCoder@users.noreply.github.com>
2021-07-04 14:25:15 -0400

    Add files via upload

---
+++ Netflix-Clone2021/Netflix Clone/Netflix Clone/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Netflix Clone
+//
+//  Created by RayQuan Kennedy on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
